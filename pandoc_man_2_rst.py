c768d0ff431ec5f80caac3865951683bfd50f087
Sara W <60634446+Snell1224@users.noreply.github.com>
2024-07-08 12:07:12 -0600

    Update pandoc_man_2_rst.py

--- pandoc_man_2_rst.py
+++ pandoc_man_2_rst.py
@@ -21,18 +21,18 @@ for c,s in enumerate(source):
         os.system(f'cp man2rst/ldms/{fname} docs/source/{dest[c]}') 
 
 
-SOS_ROOT='/opt/ovis/build/sos'
-source = ['rpc','sos/python']
-dest = ['sos/sos_man','sos/sos_man']
-for c,s in enumerate(source):
-    files = glob.glob(f'{SOS_ROOT}/{s}/*man')
-    for i in files:
-        fname = i.split('/')[-1].replace('.man','.rst')
-        os.system('mkdir -p man2rst/sos')
-        os.system(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/sos/{fname}')
-        plugin = fname.replace('.rst','')
-        plugin_title = '='*len(plugin)
-        os.system('sed -i -e "0,/man/{s/man/'+plugin+'/}" man2rst/sos/'+fname) 
-        os.system('sed -i -e "s/===/'+plugin_title+'/" man2rst/sos/'+fname) 
-        os.system(f'cp man2rst/sos/{fname} docs/source/{dest[c]}') 
+#SOS_ROOT='/opt/ovis/build/sos'
+#source = ['rpc','sos/python']
+#dest = ['sos/sos_man','sos/sos_man']
+#for c,s in enumerate(source):
+#    files = glob.glob(f'{SOS_ROOT}/{s}/*man')
+#    for i in files:
+#        fname = i.split('/')[-1].replace('.man','.rst')
+#        os.system('mkdir -p man2rst/sos')
+#        os.system(f'/usr/local/bin/pandoc -f man -s -t rst --toc {i} -o man2rst/sos/{fname}')
+#        plugin = fname.replace('.rst','')
+#        plugin_title = '='*len(plugin)
+#        os.system('sed -i -e "0,/man/{s/man/'+plugin+'/}" man2rst/sos/'+fname) 
+#        os.system('sed -i -e "s/===/'+plugin_title+'/" man2rst/sos/'+fname) 
+#        os.system(f'cp man2rst/sos/{fname} docs/source/{dest[c]}') 
 
